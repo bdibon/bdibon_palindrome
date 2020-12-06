383ef9e0b65d3f070887b2a77a3080d98545bdd4
Boris Dibon <boris.dibon@gmail.com>
2020-12-06 16:08:22 +0100

    Done

--- lib/bdibon_palindrome.rb
+++ lib/bdibon_palindrome.rb
@@ -1,6 +1,18 @@
-require "bdibon_palindrome/version"
+# require "bdibon_palindrome/version"
 
-module BdibonPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # a stub method is a method that does not work but at least exist
+  def letters
+    scan(/\w+/).join
+  end
+
+  private
+    def processed_content
+      letters.downcase
+    end
 end
